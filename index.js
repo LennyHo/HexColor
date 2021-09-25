88f8321038f6da426556b4f859f56a49c69f87c9
Lenny <kiddoyk@gmail.com>
2021-09-25 20:21:18 +0800

    hex website

---
+++ index.js
@@ -0,0 +1,34 @@
+document.getElementById("buttonClick").addEventListener("click", function() {
+    document.getElementById("words").style.color = '#000000';
+});
+
+function changeColor() {
+    var hex_numbers = [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+    ];
+
+    var hexcode = "";
+
+    for (let i = 0; i < 6; i++) {
+        var random_index = Math.floor(Math.random() * hex_numbers.length);
+        hexcode += hex_numbers[random_index];
+    }
+    document.getElementById("hex-code").innerHTML = hexcode;
+    document.getElementsByTagName("body")[0].style.backgroundColor =
+        "#" + hexcode;
+}
